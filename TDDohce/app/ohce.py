e8711c17df6a79a368867001cfc2c9ed6b7888d4
MIchael Ignacio Villanueva Torres <mvillanueva2019@inf.udec.cl>
2023-08-29 17:12:07 -0400

    Driven 1
    Se crea una salida estandar cuando la primera frase es ohce

--- TDDohce/app/ohce.py
+++ TDDohce/app/ohce.py
@@ -4,7 +4,15 @@ import datetime
 def ohce ( frase ):
 
 
-    
+    if frase.split()[0]=="ohce":
+        nombre = frase.split()[1]
+        hora_actual = datetime.datetime.now().time()
+        if hora_actual >= datetime.time(6) and hora_actual < datetime.time(12):
+            return "Buenos días "+nombre+" !"
+        elif hora_actual >= datetime.time(12) and hora_actual < datetime.time(20):
+            return "Buenas tardes "+nombre+" !"
+        else:
+            return "¡Buenas noches "+nombre+" !"
     
 
 
