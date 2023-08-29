fb39b3bfc460d998bddb505998786846c1683eee
MIchael Ignacio Villanueva Torres <mvillanueva2019@inf.udec.cl>
2023-08-29 17:11:29 -0400

    Test 1
    Funcion de buenos dias

--- TDDohce/test/test_ohce.py
+++ TDDohce/test/test_ohce.py
@@ -6,8 +6,5 @@ from app.ohce import *
 
 def test_ohce():
     
-    assert ohce1("ohce michael") == "Buenas tardes michael !"
-    assert ohce1("ohce") == "echo"
-    assert ohce1("oto") == "oto"+"\n"+"¡Bonita palabra!"
-    assert ohce1("ohce michael") == "Buenas tardes michael !"
-    assert ohce1("michael") == "leahcim"
+    assert ohce("ohce michael") == "Buenas tardes michael !"
+    
